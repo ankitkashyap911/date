cd9ef816eaa3e0134a5ab69e7c23d1f2eb1a3ac2
ankitkashyap911 <109938323+ankitkashyap911@users.noreply.github.com>
2022-09-05 11:41:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,32 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './Style.css';
+
+const App = () => {
+  let time= new Date().toLocaleTimeString();
+  const [curtime, setcurtime] = useState(time);
+
+
+  const updatedfunction=() => {
+    time = new Date().toLocaleTimeString()
+     setcurtime(time);
+    }
+    setInterval (updatedfunction,1000)
+
+  return (
+
+
+    <>
+    <div className= "counterdiv">
+    <h1 className='text-center'>
+       Time- {curtime}
+      </h1>
+
+    </div>
+    </>
+  )
+}
+
+export default App;
+
+
